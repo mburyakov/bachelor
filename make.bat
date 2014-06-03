41b5a5135217182ca89a4a00f2878d79cd41352c
Mihail Buryakov <Mihail.Buryakov@jetbrains.com>
2014-06-03 22:07:19 +0400

    clean after make

--- make.bat
+++ make.bat
@@ -1,15 +1,15 @@
 REM @echo off
-set GET="C:\Program Files (x86)\Git\bin\git.exe"
+set GIT="C:\Program Files (x86)\Git\bin\git.exe"
 if exist %1 goto Make
 echo Usage: make ThesisFile.tex
 goto End
 :Make
-git clean -dfx
+%GIT% clean -dfx
 latex %1
 bibtex8 -H -c cp1251 %~dpn1.aux
 latex %1
 latex %1
 dvips %~dpn1.dvi
 mgs -sDEVICE=pdfwrite -DNOPAUSE -DBATCH -sOutputFile=%~dpn1.pdf %~dpn1.ps
-git clean -dfx
+%GIT% clean -dfx
 :End
